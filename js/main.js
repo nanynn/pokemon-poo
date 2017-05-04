9af28138881d298d2eb16ab2f49b75beaa03b436
Anayn Pavez <nanyn.pavez@gmail.com>
2017-05-04 14:30:37 -0300

    segunda version pokemones pelean entre ellos

--- js/main.js
+++ js/main.js
@@ -10,20 +10,34 @@ function Pokemon(nombre, color, poderDeAtaque){
 	this.mostrarPokemon = function(){
 		console.log('Hola, soy:' + this.nombre + 'y soy de color: ' + this.color);
 	}
-	
+		
 	this.aumentarAmistad = function(valor){
 		this.nivelDeAmistad = this.nivelDeAmistad + valor;
 	}
 
 	this.atacar = function(pokemon){
-		pokemon.vida = pokemon.vida - this.poderDeAtaque;
+		return pokemon.vida = pokemon.vida - this.poderDeAtaque;
 	}
-
 }
 
-var Pikachu = new Pokemon ('Pikachu', 'amarillo', 100);
-var Charmander = new Pokemon('Charmander', 'rojo', 20);
+function pelear(){
+	var pokeUno = document.getElementById('pokemon-uno').value;
+	var pokeDos = document.getElementById('pokemon-dos').value;
 
-Pikachu.atacar(Charmander);
+	var ataque = parseInt(prompt("ingrese poder de ataque peleador"));
+	var ataqueDos = parseInt(prompt("ingrese poder de ataque retador"));
+	
 
-console.log(Charmander.vida);
+	var peleador = new Pokemon(pokeUno, 'color', ataque);
+	var retador = new Pokemon(pokeDos, 'color', ataqueDos);
+	
+
+	var pelea = document.getElementById('arena');
+	pelea.innerHTML = peleador.nombre +' ataco a ' + retador.nombre + ' y '+ retador.nombre +' tiene una vida ' + retador.atacar(peleador);
+
+	f(pokeUno == pokeDos){
+		alert("no puede ser el mismo pokemón");
+	}
+}
+
+
